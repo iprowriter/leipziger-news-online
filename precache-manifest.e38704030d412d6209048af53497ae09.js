9b1ea5814f58c939df78de748f0705733e607d52
iprowriter <46689083+iprowriter@users.noreply.github.com>
2020-09-01 15:00:16 +0200

    Updates

---
+++ precache-manifest.e38704030d412d6209048af53497ae09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea6655f600fc110713ca0c8985f22a3",
+    "url": "/leipziger-news-online/index.html"
+  },
+  {
+    "revision": "542fa762fc5f1b471445",
+    "url": "/leipziger-news-online/static/css/main.9f51aeaa.chunk.css"
+  },
+  {
+    "revision": "d8100702dad280bf2191",
+    "url": "/leipziger-news-online/static/js/2.427217fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leipziger-news-online/static/js/2.427217fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542fa762fc5f1b471445",
+    "url": "/leipziger-news-online/static/js/main.7beb9785.chunk.js"
+  },
+  {
+    "revision": "a2e0d5be918cc63b637a",
+    "url": "/leipziger-news-online/static/js/runtime-main.38f05f01.js"
+  },
+  {
+    "revision": "8c0290fc8e70e76f115ba2a24baa7a59",
+    "url": "/leipziger-news-online/static/media/backgroundleipzig3.8c0290fc.jpg"
+  },
+  {
+    "revision": "db4ff89e0cd9acd378b34ccf06ffbdba",
+    "url": "/leipziger-news-online/static/media/so-white.db4ff89e.png"
+  }
+]);
